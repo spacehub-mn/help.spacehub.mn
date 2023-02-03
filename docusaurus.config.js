4a967a72da7ffa7f4f6256db792a20d43d6de2d3
theironbatka <batmend22@gmail.com>
2023-02-03 14:33:24 +0800

    init

---
+++ docusaurus.config.js
@@ -0,0 +1,132 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Spacehub туслах',
+  tagline: 'Spacehub аппын заавар',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://help.spacehub.mn',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'spacehub', // Usually your GitHub org/user name.
+  projectName: 'app-support', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'mn',
+    locales: ['mn'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/'
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        // title: 'My Site',
+        logo: {
+          alt: 'Spacehub logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          // {
+          //   type: 'doc',
+          //   docId: 'intro',
+          //   position: 'left',
+          //   label: 'Заавар',
+          // },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Цахим хаягууд',
+            items: [
+              {
+                label: 'Slack',
+                href: 'https://spacehub-cs.slack.com/',
+              },
+              {
+                label: 'Facebook',
+                href: 'https://www.facebook.com/spacehub.mn',
+              },
+              {
+                label: 'Цахим хуудас',
+                href: 'https://spacehub.mn',
+              },
+            ],
+          },
+          {
+            title: 'Холбоо барих',
+            items: [
+              {
+                label: 'Цахим шуудан',
+                href: 'mailto:info@spacehub.mn',
+              },
+              {
+                label: 'Утас',
+                href: 'tel:+97677774242',
+              },
+            ],
+          },
+          {
+            title: 'Бусад',
+            items: [
+              {
+                label: 'Лого татах',
+                href: 'https://spacehub.mn/spacehub-assets.zip',
+              },
+            ],
+          },
+        ],
+        copyright: `© ${new Date().getFullYear()} Spacehub туслах, Docusaurus ашиглав.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
