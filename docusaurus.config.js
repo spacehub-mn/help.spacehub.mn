d0691973e14f4216bdb62f4a47656dfcfd0d43c0
theironbatka <batmend22@gmail.com>
2023-02-22 12:33:38 +0800

    c

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,12 +72,11 @@ const config = {
         appId: "8DVP8D0IC2",
         indexName: "netlify_f748018c-10b7-485c-a27c-3fe997fb2032_main_all",
         apiKey: "154dfc27f26a114116e6c31d66b4218c",
-        replaceSearchResultPathname: {
-          from: '/docs/',
-          to: '/',
-        },
-        externalUrlRegex: "help\\.spacehub\\.mn",
-        contextualSearch: false,
+        // replaceSearchResultPathname: {
+        //   from: '/docs/',
+        //   to: '/',
+        // },
+        externalUrlRegex: "help\\.spacehub\\.mn"
       },
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
