1f3396be554a1d1bb3cc64fcc2ff43d2272da67f
sadespresso <batmend@gege.mn>
2023-10-20 18:15:24 +0800

    heehee

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "spacehub-mn", // Usually your GitHub org/user name.
-  projectName: "app-support", // Usually your repo name.
+  projectName: "help.spacehub.mn", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "throw",
